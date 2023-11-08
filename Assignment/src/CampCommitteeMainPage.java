4bba5a3008862a5719c200bb16ab4f6a139cab48
Dylan Tan <tzsds1706@gmail.com>
2023-11-08 20:42:16 +0800

    Adding Camp Committee Pages

---
+++ Assignment/src/CampCommitteeMainPage.java
@@ -0,0 +1,79 @@
+//import java.util.Scanner;
+
+public class CampCommitteeMainPage {
+    private MainMenu mainMenu;
+    private InputScanner inputScanner;
+    private User currentUser;
+    private String userRole;
+
+    public CampCommitteeMainPage(User currentUser, String userRole) {
+        this.currentUser = currentUser;
+        this.userRole = userRole;
+        mainMenu = new StudentMainMenu();
+        inputScanner = new InputScanner();
+
+    }
+
+    public void run() {
+        boolean continueMenu = true;
+        while (continueMenu) {
+            System.out.println("Welcome " + currentUser.getName());
+            mainMenu.displayMainMenu();
+            int option = inputScanner.promtForInt("Input your choice of action (1-7):");
+            switch (option) {
+
+                case 1:
+                    // Navigate to Faculty Information page
+                    System.out.println("Navigating to Faculty Information page...");
+                    break;
+                case 2:
+                    // Display status as committee
+                    System.out.println("Checking your status as committee...");
+                    break;
+                case 3:
+                    // Display list of camps available
+                    System.out.println("Retrieving list of available camps...");
+                    break;
+                case 4:
+                    // Navigate to Enquiry Page
+                    System.out.println("Navigating to Enquiry Page...");
+                    break;
+                case 5:
+                    // Display registered camps
+                    System.out.println("Retrieving registered camps...");
+
+                    break;
+                case 6:
+                    // Initiate password change process
+                    System.out.println("Submitting Suggestions...");
+                    continueMenu = false;
+                    return;
+                case 7:
+                    System.out.println("Viewing Enquiries...");
+                    continueMenu = false;
+                    return;
+                case 8:
+                    System.out.println("Viewing Suggestions...");
+                    continueMenu = false;
+                    return;
+                case 9:
+                    System.out.println("Generating Report...");
+                    continueMenu = false;
+                    return;
+                case 10:
+                    System.out.println("Initiating password change process...");
+                    ChangeAccountPassword.changePassword();
+                    System.out.println("Returning back to main page...");
+                    break;
+                case 11:
+                    System.out.println("Logging out...");
+                    continueMenu = false;
+                    return;
+                default:
+                    System.out.println("Invalid option. Please enter a valid option.");
+                    break;
+            }
+        }
+
+    }
+}
