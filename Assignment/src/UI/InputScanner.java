0f09bb9c11f8aa58ebe496a16c5f8666ee17aca3
Matthew Tan <matthewtanyh@gmail.com>
2023-11-17 23:35:37 +0800

    inputscanner

--- Assignment/src/UI/InputScanner.java
+++ Assignment/src/UI/InputScanner.java
@@ -1,15 +1,24 @@
 package UI;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputScanner {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int promptForInt(String prompt) {
-        System.out.print(prompt);
-        int x = scanner.nextInt();
-        scanner.nextLine(); //This is to clear the '\n' left in the input stream
-        return x;
+        while (true){
+            try{
+                System.out.print(prompt);
+                int x = scanner.nextInt();
+                scanner.nextLine(); //This is to clear the '\n' left in the input stream
+                return x;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Wrong input entered. Please try again");
+                InputScanner.clear();
+            }
+        }
     }
 
     public static String promptForString(String prompt) {
--- Assignment/src/UI/SuggestionMainPage.java
+++ Assignment/src/UI/SuggestionMainPage.java
@@ -1,7 +1,5 @@
 package UI;
 
-import java.util.InputMismatchException;
-
 import Controller.Account.LoginManager;
 import Controller.Suggestion.SuggestionManager;
 import Entity.CampCommittee;
@@ -12,48 +10,42 @@ public class SuggestionMainPage {
         CampCommittee currentUser = (CampCommittee)LoginManager.getCurrentUser();
         while (continueMenu){
             SuggestionMainMenu.displaySuggestionMenu();
-            try{
-                int option = InputScanner.promptForInt("Input your choice of action (1-4): ");
-                switch(option){
-                    case 1:
-                        //View Suggestion
-                        System.out.println("Viewing Suggestions...");
-                        SuggestionManager.printSuggestions(SuggestionManager.getSuggestions(currentUser.getSuggestions(), true));
-                        break;
+            int option = InputScanner.promptForInt("Input your choice of action (1-4): ");
+            switch(option){
+                case 1:
+                    //View Suggestion
+                    System.out.println("Viewing Suggestions...");
+                    SuggestionManager.printSuggestions(SuggestionManager.getSuggestions(currentUser.getSuggestions(), true));
+                    break;
 
-                    case 2:
-                        //Give Suggestion
-                        SuggestionManager.addSuggestion();
-                        break;
+                case 2:
+                    //Give Suggestion
+                    SuggestionManager.addSuggestion();
+                    break;
 
-                    case 3:
-                        //Edit Suggestion
-                        SuggestionManager.editSuggestion(currentUser.getSuggestions());
-                        break;
+                case 3:
+                    //Edit Suggestion
+                    SuggestionManager.editSuggestion(currentUser.getSuggestions());
+                    break;
 
-                    case 4:
-                        //Delete Suggestion
-                        SuggestionManager.deleteSuggestion(currentUser.getSuggestions());
-                        break;
-                    case 5:
-                        //View processed suggestions
-                        SuggestionManager.printSuggestions(SuggestionManager.getSuggestions(currentUser.getSuggestions(), false));
-                        break;
-                    case 6:
-                        //Back to MM
-                        System.out.println("Returning to main menu");
-                        continueMenu = false;
-                        break;
+                case 4:
+                    //Delete Suggestion
+                    SuggestionManager.deleteSuggestion(currentUser.getSuggestions());
+                    break;
+                case 5:
+                    //View processed suggestions
+                    SuggestionManager.printSuggestions(SuggestionManager.getSuggestions(currentUser.getSuggestions(), false));
+                    break;
+                case 6:
+                    //Back to MM
+                    System.out.println("Returning to main menu");
+                    continueMenu = false;
+                    break;
 
-                    default:
-                        System.out.println("Invalid option. Please enter a valid option");
-                }
-                System.out.println();
+                default:
+                    System.out.println("Invalid option. Please enter a valid option");
             }
-            catch(InputMismatchException e){
-                System.out.println("Invalid option. Please enter a valid option");
-                InputScanner.clear();
-            }
+            System.out.println();
         }
     }
 }
