c3c67fde9b0347c8a346552b6b35cf80bdd02925
Matthew Tan <matthewtanyh@gmail.com>
2023-11-21 00:13:42 +0800

    update resetcsv

--- Assignment/src/Controller/Reset.java
+++ Assignment/src/Controller/Reset.java
@@ -1,26 +1,19 @@
 package Controller;
-import java.util.InputMismatchException;
 
 import Controller.Utilities.InputScanner;
 
 public class Reset {
     public static int PromptForReset(){
         int choice;
+        System.out.println("1. Reset data");
+        System.out.println("2. Use previous data");
         while (true){
-            try{
-                System.out.println("1. Reset data");
-                System.out.println("2. Use previous data");
-                choice = InputScanner.promptForInt("Input your choice here: ");
-                if (choice != 1 && choice != 2){
-                    System.out.println("Please enter the values 1 or 2 only");
-                    continue;
-                }
-                return choice;
+            choice = InputScanner.promptForInt("Input your choice here: ");
+            if (choice != 1 && choice != 2){
+                System.out.println("Please enter the values 1 or 2 only");
+                continue;
             }
-            catch(InputMismatchException e){
-                System.out.println("Invalid input. Please enter a integer instead:");
-                InputScanner.clear();
-            }
+            return choice;
         }
     }
 }
