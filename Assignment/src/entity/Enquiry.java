7a709ffcfb0b154c970f5136bb6e61b10153525d
Dylan Tan <tzsds1706@gmail.com>
2023-11-26 02:39:56 +0800

    Entity javadoc

--- Assignment/src/entity/Enquiry.java
+++ Assignment/src/entity/Enquiry.java
@@ -1,17 +1,43 @@
 package entity;
 
+/**
+ * This class represents an enquiry made by a user towards a certain camp
+ * @author SCSZ Group 4
+ * @version 1.0
+ * @since 25/11/23
+ */
 public class Enquiry {
+     /** Status of the inquiry. */
     public enum Status {
-        REPLIED, PENDING
+        /** The enquiry has been replied to. */
+        REPLIED,
+        
+        /** The enquiry is pending and has not been replied to yet. */
+        PENDING
     };
 
+    /** The sender of the enquiry. */
     private String sender;
+    /** The status of the enquiry (replied or pending). */
     private Status status;
+     /** The content of the enquiry. */
     private String content;
+    /** The replier who replied to the enquiry. */
     private String replier;
+    /** The name of the camp related to the enquiry. */
     private String campName;
+    /** The content of the reply to the enquiry (if replied). */
     private String repliedContent;
 
+
+    /**
+     * Constructs a new Enquiry object with the specified sender, content, and camp name.
+     *
+     * @param sender - the sender of the enquiry
+     * @param content - the content of the enquiry
+     * @param campName - the name of the camp related to the enquiry
+     * @param status - the status of the enquiry default will be set to PENDING
+     */
     public Enquiry(String sender, String content, String campName) {
         this.sender = sender;
         this.content = content;
@@ -19,50 +45,98 @@ public class Enquiry {
         this.status = Status.PENDING;
     }
 
+     /**
+     * Sets the status of the enquiry.
+     * @param status - the new status of the enquiry
+     */
     public void setStatus(Status status) {
         this.status = status;
     }
 
+    /**
+     * Retrieves the status of the enquiry.
+     * @return the status of the enquiry
+     */
     public Status getStatus() {
         return status;
     }
 
+    /**
+     * Sets the content of the enquiry.
+     * @param content - the new content of the enquiry
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Retrieves the content of the enquiry.
+     * @return the content of the enquiry
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Retrieves the sender of the enquiry.
+     * @return the sender of the enquiry
+     */
     public String getSender() {
         return sender;
     }
 
+    /**
+     * Sets the replier of the enquiry.
+     * @param replier - the new replier of the enquiry
+     */
     public void setReplier(String replier) {
         this.replier = replier;
     }
 
+    /**
+     * Retrieves the replier of the enquiry.
+     * @return the replier of the enquiry
+     */
     public String getReplier() {
         return replier;
     }
 
+    /**
+     * Checks whether an enquiry has been replied or not
+     * @return {@code true} if the enquiry has been replied, {@code false} otherwise
+     */
     public boolean hasReplied() {
         return status == Status.REPLIED;
     }
 
+    /**
+     * Retrieves the camp name of the enquiry.
+     * @return the camp name of the enquiry
+     */
     public String getCampName() {
         return campName;
     }
 
+    /**
+     * Sets the camp name of the enquiry.
+     * @param campName - the new camp name of the enquiry
+     */
     public void setCampName(String campName) {
         this.campName = campName;
     }
 
+    /**
+     * Retrieves the replied content of the enquiry.
+     * @return the replied content of the enquiry
+     */
     public String getRepliedContent(){
         return repliedContent;
     }
 
+    /**
+     * Sets the replied content of the enquiry.
+     * @param repliedContent - the new replied content of the enquiry
+     */
     public void setRepliedContent(String repliedContent){
         this.repliedContent = repliedContent;
     }
--- Assignment/src/entity/Suggestion.java
+++ Assignment/src/entity/Suggestion.java
@@ -1,34 +1,81 @@
 package entity;
 
-public class Suggestion {
-    public enum Status {APPROVED, PENDING, REJECTED};
+/**
+ * This class represents a suggestion made by a camp committee member.
+ * @author SCSZ Group 4
+ * @version 1.0
+ * @since 25/11/23
+ */
 
+public class Suggestion {
+    /** Enumerated type representing the status of the suggestion. */
+    public enum Status {
+        /** The suggestion has been approved. */
+        APPROVED,
+        
+        /** The suggestion is pending and has not been processed yet. */
+        PENDING,
+        
+        /** The suggestion has been rejected. */
+        REJECTED
+    };
+
+    /** The camp committee member who made the suggestion. */
     private CampCommittee proposer;
+    /** The status of the suggestion (approved, pending, or rejected). */
     private Status status;
+    /** The content of the suggestion. */
     private String content;
 
+    /**
+     * Constructs a new Suggestion object with the specified proposer and content.
+     * The status is set to {@code PENDING} by default.
+     *
+     * @param proposer - the camp committee member who made the suggestion
+     * @param content - the content of the suggestion
+     */
     public Suggestion(CampCommittee proposer, String content){
         this.proposer = proposer;
         this.content = content;
         this.status = Status.PENDING;
     }
 
+     /**
+     * Sets the status of the suggestion.
+     * @param status - the new status of the suggestion
+     */
     public void setStatus(Status status){
         this.status = status;
     }
 
+    /**
+     * Retrieves the status of the suggestion.
+     * @return the status of the suggestion
+     */
     public Status getStatus(){
         return status;
     }
 
+    /**
+     * Sets the content of the suggestion.
+     * @param status - the new content of the suggestion
+     */
     public void setContent(String content){
         this.content = content;
     }
 
+    /**
+     * Retrieves the content of the suggestion.
+     * @return the content of the suggestion
+     */
     public String getContent(){
         return content;
     }
 
+    /**
+     * Retrieves the camp committee member who made the suggestion.
+     * @return the proposer of the suggestion
+     */
     public CampCommittee getProposer(){
         return proposer;
     }
