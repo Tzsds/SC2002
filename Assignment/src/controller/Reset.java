cdf5f19106f88068d90daacc864aa284d1ebd4f0
boonyii <laiboonyi@hotmail.com>
2023-11-26 02:35:00 +0800

    reset javadoc

--- Assignment/src/controller/Reset.java
+++ Assignment/src/controller/Reset.java
@@ -2,9 +2,26 @@ package controller;
 
 import controller.utils.InputScanner;
 
+/**
+ * The class responsible for prompting the user for data reset options
+ * It provides a method to display reset option menu and receive user input for choosing whether
+ * to reset data or use previous data
+ * @author SCSZ Group 4
+ * @version 1.0 
+ * @since 25/11/2023
+ */
 public class Reset {
+
+    /**
+     * Prompts the user to choose between resetting data and using previous data
+     * 
+     * @return An integer representing the user's choice:
+     *         - 1 for resetting data
+     *         - 2 for using previous data
+     */
     public static int PromptForReset(){
         int choice;
+        // Display reset option menu
         System.out.println("1. Reset data");
         System.out.println("2. Use previous data");
         while (true){
